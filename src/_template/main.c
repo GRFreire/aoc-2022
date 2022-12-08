42844777606f94e5458b57b8f109fda3cb1879ad
Guilherme Rugai Freire <dev@grfreire.com>
2022-12-08 20:49:33 -0300

    add a template folder

---
+++ src/_template/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#if 0 
+#define INPUT_FILE "input.txt"
+#else
+#define INPUT_FILE "example.txt"
+#endif
+
+#define PART 1
+
+
+void part1(void) {
+}
+
+void part2(void) {
+}
+
+
+int main(void) {
+#if PART == 1
+  part1();
+#else
+  part2();
+#endif
+}
+
